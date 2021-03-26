86a463894450b76c53ccba021b8fb6b7b722a1f5
Narella Calvente <narellacalvente@gmail.com>
2021-03-26 17:01:16 -0300

    BQA-08: variable creation

--- VariablesAndOperators/index.js
+++ VariablesAndOperators/index.js
@@ -1,3 +1,26 @@
-var name = "VariablesAndOperators";
+// a)
 
-console.log(name); 
+var ageNarella = 25;
+var ageMariana = 25;
+
+var addAges = ageNarella + ageMariana;
+
+console.log(addAges);
+
+// b)
+
+var name = 'Narella ';
+var lastName = 'Calvente';
+
+var concatenate = name.concat(lastName);
+
+console.log(concatenate);
+
+// c)
+
+var city = 'Rosario';
+var country = 'Argentina';
+
+var concat =  city.concat(' ').concat(country);
+
+console.log(concat.length);
