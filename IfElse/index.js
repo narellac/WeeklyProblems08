a2d6bb2884b346cad31f521133c7d6df8eed24d0
Narella Calvente <narellacalvente@gmail.com>
2021-03-27 10:45:46 -0300

    BQA-08: variable creation

--- IfElse/index.js
+++ IfElse/index.js
@@ -1,3 +1,32 @@
-var name = "ifElse";
+// a)
 
-console.log(name);
+var random = Math.random();
+
+console.log(random);
+
+if (random >=0,5){
+  console.log('Greater than 0,5');
+}else{
+  console.log('Lower than 0,5');
+};
+
+// b)
+
+var age = 27;
+
+
+if (age<2){
+  console.log('Baby');
+}else if (age>1 && age<13){
+  console.log('Kid');
+}else if (age>12 && age<20){
+  console.log('Teen');
+}else if (age>19 && age<31){
+  console.log('Young');
+}else if (age>30 && age<61){
+  console.log('Adult');
+}else if (age>60 && age<76){
+  console.log('Elderly');
+}else{
+console.log('Old man');
+};
